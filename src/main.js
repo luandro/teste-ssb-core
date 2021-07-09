fa2d3770de44bd61ab50c4e290d37724c8920c24
luandro <luandro@gmail.com>
2021-07-09 15:18:26 -0300

    fix: ssb-core

--- src/main.js
+++ src/main.js
@@ -2,22 +2,27 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 let SSB
+let err
 if (window.getSSBSingleton) {
   const ssbSingleton = window.getSSBSingleton();
-  SSB = ssbSingleton.getSSB();
+  [err, SSB] = ssbSingleton.getSSB();
 } else {
   console.log('Bundle not loaded!')
 }
+console.log('SSB', SSB, err)
 
-SSB.db.publish(
-  {
-    type: "post",
-    text: "oioi",
-  },
-  (err) => {
-    console.log("🚀 ~ file: main.js ~ line 12 ~ err", err);
-    console.log("Postado");
-  }
-);
+if (SSB) {
+  console.log('Postando')
+  SSB.db.publish(
+    {
+      type: "post",
+      text: "oioi",
+    },
+    (err) => {
+      console.log("🚀 ~ file: main.js ~ line 12 ~ err", err);
+      console.log("Postado");
+    }
+  );
+}
 
 createApp(App).mount("#app");
